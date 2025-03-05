0d80eb159aaea2993f323875cca1deca884689b5
Nemanja Planic <planicnemanja@gmail.com>
2025-03-05 19:15:22 +0100

    First implementation of fligths filtering

--- glavna/urls.py
+++ glavna/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.pocetna)]
+    path('', views.pocetna),
+    path('pretraga/', views.pretraga, name='rezpretrage'),
+    ]
--- glavna/views.py
+++ glavna/views.py
@@ -5,3 +5,23 @@ def pocetna(req):
     letovi = Let.objects.all()
     gradovi = Grad.objects.all()
     return render(req,'letovi.html',{'letovi':letovi,'gradovi':gradovi})
+
+# def pretraga(req):
+#     iz = req.GET.get('od')
+#     dest = req.GET.get('do')
+#     letovi = Let.objects.filter(od__naziv=iz,do__naziv=dest)
+#     return render(req,'letovi.html',{'letovi':letovi})
+
+def pretraga(req):
+    iz = req.GET.get('od')
+    dest = req.GET.get('do')
+
+    print(f"Polaziste iz forme: {iz}")
+    print(f"Destinacija iz forme: {dest}")
+
+    letovi = Let.objects.filter(od__naziv=iz, do__naziv=dest)
+
+    print(f"SQL Upit: {letovi.query}")  # Prikazuje SQL koji Django generiše
+
+    return render(req, 'letovi.html', {'letovi': letovi})
+
